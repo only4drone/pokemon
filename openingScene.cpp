3594d0c469ee458138e5ac8c9fbd8d8bbb12bf56
only4drone <ljh201410@gmail.com>
2021-09-02 16:48:45 +0900

    엔딩씬 이미지 추가, 오프닝 씬 코드 살짝 첨가(주석 처리해서 안 그려짐)

--- openingScene.cpp
+++ openingScene.cpp
@@ -11,6 +11,8 @@ openingScene::~openingScene()
 
 HRESULT openingScene::init()
 {
+	opening = IMAGEMANAGER->addFrameImage("¿ÀÇÁ´×", "image/op_image_frame.bmp", 65280, 576, 102, 1, true, RGB(255, 0, 255));
+
 	return S_OK;
 }
 
@@ -20,8 +22,10 @@ void openingScene::release()
 
 void openingScene::update()
 {
+	_index++;
 }
 
 void openingScene::render()
 {
+	opening->frameRender(getMemDC(), 0,0, _index, 0);
 }
